278de39f5119616780e6d61bae9a31bff4f15d4d
kotlj <crotlj6@gmail.com>
2023-12-19 10:36:49 +0200

    done

---
+++ HW_WPF_SmoothPic/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HW_WPF_SmoothPic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HW_WPF_SmoothPic/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_WPF_SmoothPic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// ;component
+    ///  System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
+    ///     objBlur.Radius = 10;
+    ///     win.Effect = objBlur;
+    public partial class MainWindow : Window
+    {
+        System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeSmooth(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            objBlur.Radius = Smooth.Value;
+            Pic.Effect = objBlur;
+        }
+    }
+}
